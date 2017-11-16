e0c45f12cf4b5c03f49c2f26b07170a4f0291971
ppsari <holie.nait@gmail.com>
2017-11-16 13:01:02 +0700

    property search & changeStatus (tambah props), login

--- server/schema/user360Schema.js
+++ server/schema/user360Schema.js
@@ -1,4 +1,6 @@
 const User360 = require('../models/user360')
+const login = require('../helpers/login')
+
 
 const {
   GraphQLObjectType,
@@ -21,6 +23,7 @@ const UserInputType = new GraphQLInputObjectType({
   name: 'UserInputType',
   fields: {
     phone: {type: GraphQLString},
+    password: {type: GraphQLString},
     name: {type: GraphQLString}
   }
 })
@@ -51,6 +54,27 @@ const user = {
   })
 }
 
+const loginuser = {
+  type: UserType,
+  args: {
+    password: {name: 'password', type: GraphQLString},
+    phone: {name:'phone', type: GraphQLString}
+  },
+  resolve: (root, args) => new Promise((resolve, reject) => {
+    const {password, phone} = args
+
+    if (password === null) reject({errors:{"message": "Password must be filled"}})
+
+    User360.findOne({phone:phone}, (err, f_user) => {
+      if (err) reject(err)
+      else if (!login.checkPassword(password,f_user.password)) reject({errors:{"message": "Invalid Email/Password"}})
+      else resolve(f_user)
+    })
+  })
+}
+
+
+
 const createUser = {
   type: UserType,
   args: {
@@ -63,7 +87,8 @@ const createUser = {
     const {input} = args
     let n_user = new User360({
       phone: input.phone,
-      name: input.name
+      name: input.name,
+      password: input.password
     })
     n_user.save((err, s_user) => err? reject(err.errors) : resolve(s_user) )
   })
@@ -103,13 +128,18 @@ const deleteUser = {
       if (err) reject(err)
       else user.remove((err, d_user)=> err? reject(err) : resolve(d_user) )
     })
+
+    // User360.remove({}, (err) => {
+    //   reject(err)
+    // })
   })
 }
 
 module.exports = {
   user,
   users,
+  loginuser,
   createUser,
   updateUser,
-  deleteUser
+  deleteUser,
 }
