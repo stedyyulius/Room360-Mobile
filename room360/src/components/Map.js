0572a41298c068f7953b866263fd6abc353c0c7e
Stedy Yulius <stedyyulius@gmail.com>
2017-11-17 11:20:39 +0700

    change kos location

--- room360/src/components/Map.js
+++ room360/src/components/Map.js
@@ -24,7 +24,7 @@ const dummy = [{
   image: 'https://i.ytimg.com/vi/Xx6t0gmQ_Tw/maxresdefault.jpg',
   price: '2.000.000/bulan',
   lat: defaultLocation.lat,
-  lng: defaultLocation.lng + 0.0029,
+  lng: defaultLocation.lng - 0.0029,
   type: 'kos'
 },{
   address: 'Jl hello world',
